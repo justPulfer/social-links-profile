357f9d29e7ceba0691e0e23c9bff5654e44e7d26
just_pulfer <djlydia.pd@gmail.com>
2024-02-17 13:36:44 -0600

    added basic structure and some css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
 import "./index.css";
 
+const skills = [
+	"GitHub",
+	"Frontend Mentor",
+	"LinkedIn",
+	"Twitter",
+	"Instagram",
+];
 export default function App() {
 	return (
-		<div className="container">
-			Jessica Randall London, United Kingdom "Front-end developer and avid
-			reader." GitHub Frontend Mentor LinkedIn Twitter Instagram
+		<div>
+			<Main />
 			<Footer />
 		</div>
 	);
 }
 
+function Main() {
+	return (
+		<main>
+			<div className="container">
+				<div className="profile-info">
+					<img src="/images/avatar-jessica.jpeg" alt="Profile"></img>
+					<p className="name">Jessica Randall</p>
+					<p className="location">London, United Kingdom</p>
+					<p className="role">"Front-end developer and avid reader."</p>
+				</div>
+				<Skills />
+			</div>
+		</main>
+	);
+}
+
+function Skills() {
+	return (
+		<ul className="skills">
+			{skills.map((skill) => (
+				<li className="skill" key={skill}>
+					{skill}
+				</li>
+			))}
+		</ul>
+	);
+}
+
 function Footer() {
 	return (
-		<div class="attribution">
+		<div className="attribution">
 			Challenge by
 			<a
 				href="https://www.frontendmentor.io?ref=challenge"
