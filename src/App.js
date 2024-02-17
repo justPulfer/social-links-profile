f8fedbe9cb77bdedf25f241ca118987de720db2a
Pulfer Dadson <just_pulfer@Trudys-iMac.gateway.sasktel.net>
2024-02-17 13:04:18 -0800

    finalized

--- src/App.js
+++ src/App.js
@@ -17,18 +17,16 @@ export default function App() {
 
 function Main() {
 	return (
-		<main>
-			<div className="container">
-				<div className="profile-info">
-					<img src="/images/avatar-jessica.jpeg" alt="Profile"></img>
-					<div className="personal-info">
-						<h1 className="name">Jessica Randall</h1>
-						<h2 className="location">London, United Kingdom</h2>
-					</div>
-					<h3 className="role">"Front-end developer and avid reader."</h3>
+		<main className="container">
+			<div className="profile-info">
+				<img src="/images/avatar-jessica.jpeg" alt="Profile"></img>
+				<div className="personal-info">
+					<h1 className="name">Jessica Randall</h1>
+					<h2 className="location">London, United Kingdom</h2>
 				</div>
-				<Skills />
+				<h3 className="role">"Front-end developer and avid reader."</h3>
 			</div>
+			<Skills />
 		</main>
 	);
 }
